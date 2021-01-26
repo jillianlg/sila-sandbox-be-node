91c9ae2bee955bf0b93b68fed6fdd4d9436f3e28
Victor Bofill <victorm.bofill@gmail.com>
2021-01-26 14:56:11 -0800

    added a note to .env.example

--- .env.example.js
+++ .env.example.js
@@ -1,3 +1,7 @@
+// create a .env.js file in the home directory (sibling to index.js)
+// and include these variables within
+// ** DO NOT COMMIT NOR SHARE YOUR APP PRIVATE KEY **
+
 const APP_HANDLE = 'YOUR_APP_HANDLE_HERE';
 const APP_PRIVATE_KEY = 'YOUR_APP_PRIVATE_KEY_HERE';
 const ENCRYPTION_KEY = 'YOUR_ENCRYPTION_KEY_HERE'; // must be 32 characters
